94116b9a77f4a92d5542a898be9e7a27b5bb168f
Tarcísio Queiroz <tarcisiodrum1516378@gmail.com>
2026-08-25 18:46:04 -0300

    feat(components): Update MacroGrid Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -28,7 +28,7 @@ export default function HomeScreen() {
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <MacroGrid />
+      <MacroGrid meals={meals}/>
       <RecentMeals meals={meals} />
     </ScrollView>
   );

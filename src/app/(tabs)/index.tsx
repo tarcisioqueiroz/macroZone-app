ae6a3eb289daaa436fdc2c2b42a2ae278c6a0c76
Tarcísio Queiroz <tarcisiodrum1516378@gmail.com>
2026-08-25 18:37:58 -0300

    feat(components): Update RecenteMeals Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -29,7 +29,7 @@ export default function HomeScreen() {
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
       <MacroGrid />
-      <RecentMeals />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
--- src/components/RecentMeals.tsx
+++ src/components/RecentMeals.tsx
@@ -1,32 +1,31 @@
 import { Text, View } from "react-native";
 import { globalStyles } from "@/styles/global";
 import MealItem from "./MealItem";
+import { Meal } from "@/storage/meals";
 
-export default function RecentMeals() {
+type RecentMealsProps = {
+    meals: Meal[];
+};
+
+export default function RecentMeals({ meals }: RecentMealsProps) {
     return (
         <View style={{ marginTop: 30 }}>
             <Text style={globalStyles.sectionTitle}>Recent Meals</Text>
-            <MealItem
-                name='Chicken & Rice'
-                calories={540}
-                protein={45}
-                carbs={50}
-                fat={12}
-            />
-            <MealItem
-                name='Protein Shake'
-                calories={280}
-                protein={30}
-                carbs={20}
-                fat={8}
-            />
-            <MealItem
-                name='Salmon Salad'
-                calories={430}
-                protein={35}
-                carbs={10}
-                fat={25}
-            />
+            {meals.length === 0 ? (
+                <Text style={globalStyles.empty}>No meals logged yet.</Text>
+            ) : (
+                meals.slice(0, 5).map((meal) =>
+                    <MealItem
+                        key={meal.id}
+                        name={meal.name}
+                        calories={meal.calories}
+                        protein={meal.protein}
+                        carbs={meal.carbs}
+                        fat={meal.fat}
+                    />
+
+                )
+            )}
         </View>
     );
 }

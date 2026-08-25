dbe0c8d1068418e882cf96d0c0a9980d289f7216
Tarcísio Queiroz <tarcisiodrum1516378@gmail.com>
2026-08-25 18:26:37 -0300

    feat(storage): Get Meals From Storage In HomePage useFocusEffect

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,29 @@
-import { Text, ScrollView } from "react-native";
-import { globalStyles } from "@/styles/global";
 import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
+import { getMeals, Meal } from "@/storage/meals";
+import { Text, ScrollView } from "react-native";
+import { globalStyles } from "@/styles/global";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+
 
 export default function HomeScreen() {
+
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log("Loaded meals:", data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, [])
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
